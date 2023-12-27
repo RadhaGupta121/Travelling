aa198fe27dfd5ac77580a73130366fccf448c5fd
Radha Gupta <106135850+RadhaGupta121@users.noreply.github.com>
2023-12-27 12:27:26 +0530

    Update Profile.js

--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect,useRef, useState} from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
